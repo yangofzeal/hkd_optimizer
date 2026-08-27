d1c838721430a9759183d7bdd43a787fa5e7bc8e
yangofzeal <yangofzeal@gmail.com>
2026-08-27 09:26:04 -0400

    Delete certificate.py

--- certificate.py
+++
@@ -1,5 +0,0 @@
-# generated file -- do not edit
-import base64 as _b64, zlib as _zlib
-_src = _zlib.decompress(_b64.b85decode('c-l40K?=e!5JusBP7%7wfM!utq~J-!MQr{w12HX=iFka4;@f|ZU)w5bb7;Bn)0l{4XNYp*IVjval#YWfsEg*?c%sSJUjpq?|ID`>#`DDqHrWMv)On3?L)Ub?4n#iV6L~>XNB4=Vy8THH^992lEPV')).decode("utf-8")
-exec(compile(_src, '/mnt/data/hkd_optimizer_release_py343_portable/source/hkd_optimizer/certificate.py.src.py', "exec"))
-del _src
