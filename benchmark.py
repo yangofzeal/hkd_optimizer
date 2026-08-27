e301cadfa1721217acb29d94ff3343a00f682397
yangofzeal <yangofzeal@gmail.com>
2026-08-27 09:25:56 -0400

    Delete benchmark.py

--- benchmark.py
+++
@@ -1,5 +0,0 @@
-# generated file -- do not edit
-import base64 as _b64, zlib as _zlib
-_src = _zlib.decompress(_b64.b85decode('c-nQ3O$x#=5QX<S#UoH?MPiCOv1cjL%wW-`B}pO2H#QXv;#+>apBY08o<i?wn62@3Y%T&x5gqtB-$EiA7&);kw9>lGmbV;*)mg|&of)f_s8B~7(CSS}7vWavKhdd-esYCpvqT|GI@vEoW?gDWOpfe8y+7jqFumXJPm6XA8U1YL$K+^P7CV`Lbv8KdFn5a=#wby$')).decode("utf-8")
-exec(compile(_src, '/mnt/data/hkd_optimizer_release_py343_portable/source/hkd_optimizer/benchmark.py.src.py', "exec"))
-del _src
