507d92b76999c0c1b031c813e3b9c1a7f94d8829
yangofzeal <yangofzeal@gmail.com>
2026-08-27 09:26:14 -0400

    Delete dependency.py

--- dependency.py
+++
@@ -1,5 +0,0 @@
-# generated file -- do not edit
-import base64 as _b64, zlib as _zlib
-_src = _zlib.decompress(_b64.b85decode('c-nQ4(F(#a3`O7f6=6>;41E^_LI0!7HkC+8N1Ms!*W1a^sWVTdIl1>HHncgz2|HwmY{z3{RPlU8D|%N!DTf}6Ato_KWemQ75HDogU^3mYfz}G$un>+c=6PzW3EnJO*aNYs<(K}XKeY-keHTzoQ-cN{6AVS-+p>C!9J&-cOftS=&)DSiOuopLQ2N9WkkyLuU&Sn`w`H=VPw2{%BAek`oIG%pwT<4)B7_gI6;%`aT34&RpG#`k&aHnN=;{GD44Sh')).decode("utf-8")
-exec(compile(_src, '/mnt/data/hkd_optimizer_release_py343_portable/source/hkd_optimizer/dependency.py.src.py', "exec"))
-del _src
